56ecb563af7fcf2f31422675250c3e80d8481e43
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-04-19 07:03:49 +0200

    feature - project base

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <app-header></app-header>
+  <router-view class="container"></router-view>
+  <app-footer></app-footer>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, AppHeader, AppFooter
+  }
+}
+</script>
+
+
+<style>
+#app {
+  box-sizing: border-box;
+  color: #37474f;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin: 0 auto;
+  padding: 0;
+}
+
+.container {
+  padding-bottom: 4rem;
+}
+
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <span>Application made by <a href="https://github.com/zaquielverse">Zaquiel Rodriguez Arce</a></span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'app-footer'
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  box-sizing: border-box;
+  background-color: #cfd8dc;
+  bottom: 0;
+  left: 0;
+  padding: 1rem;
+  position: absolute;
+  width: 100%;
+
+  a {
+    text-decoration: none;
+    color: #546E7A;
+  }
+}
+</style>
